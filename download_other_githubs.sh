4fd1673fc9398d53d6942bb960dcff335c1b9d8d
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-14 10:01:52 +0200

    No need to download RibiLibraries

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -6,9 +6,9 @@ if [ ! -d RibiClasses ]; then
   git clone https://github.com/richelbilderbeek/RibiClasses
 fi
 
-if [ ! -d RibiLibraries ]; then
-  git clone https://github.com/richelbilderbeek/RibiLibraries
-fi
+#if [ ! -d RibiLibraries ]; then
+#  git clone https://github.com/richelbilderbeek/RibiLibraries
+#fi
 
 if [ ! -d RibiUnits ]; then
  git clone https://github.com/richelbilderbeek/RibiUnits
