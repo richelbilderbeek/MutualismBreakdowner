06ca8e714783dfe78077d546128fb591ddcf3ced
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-14 10:06:26 +0200

    Progress in crosscompiling [skip ci]

--- crosscompiletowindows.sh
+++ crosscompiletowindows.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #From http://richelbilderbeek.nl/CppQtCrosscompileToWindowsExample15.htm
-i686-w64-mingw32.static-qmake-qt5 ToolMutualismBreakdownerDesktop.pro
-make
+i686-w64-mingw32.static-qmake-qt5 MutualismBreakdownerDesktop.pro
+make --silent
 echo "Done"
