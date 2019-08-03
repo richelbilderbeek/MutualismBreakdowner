fc82445c482759307a056f852a19ed91b5faacc3
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-03 13:48:23 +0200

    Remove pragma's

--- mutualismbreakdownermenudialog.cpp
+++ mutualismbreakdownermenudialog.cpp
@@ -23,7 +23,7 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 //From http://www.richelbilderbeek.nl/ToolMutualismBreakdowner.htm
 //---------------------------------------------------------------------------
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weffc++"
+
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #include "mutualismbreakdownermenudialog.h"
 
